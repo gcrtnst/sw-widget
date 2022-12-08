fafbe7dc93b724c92af823fe2fa46736f2aff3fd
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2022-12-09 00:12:24 +0900

    Use `"double quote"` instead of `'single quote'`

--- widget/script.lua
+++ widget/script.lua
@@ -1,19 +1,19 @@
-c_cmd = '?widget'
+c_cmd = "?widget"
 c_spd_unit_tbl = {
-    ['km/h'] = 216,
-    ['kph'] = 216,
-    ['kmph'] = 216,
-    ['km/hr'] = 216,
+    ["km/h"] = 216,
+    ["kph"] = 216,
+    ["kmph"] = 216,
+    ["km/hr"] = 216,
 
-    ['m/s'] = 60,
-    ['mps'] = 60,
+    ["m/s"] = 60,
+    ["mps"] = 60,
 
-    ['kt'] = 216000.0/1852.0,
-    ['kn'] = 216000.0/1852.0,
+    ["kt"] = 216000.0/1852.0,
+    ["kn"] = 216000.0/1852.0,
 }
 c_alt_unit_tbl = {
-    ['m'] = 1,
-    ['ft'] = 1.0/0.3048,
+    ["m"] = 1,
+    ["ft"] = 1.0/0.3048,
 }
 g_userdata = {}
 g_usertemp = {}
@@ -26,24 +26,24 @@ function onCustomCommand(full_message, user_peer_id, is_admin, is_auth, cmd, ...
 
     local args = {}
     for _, s in ipairs({...}) do
-        if s ~= '' then
+        if s ~= "" then
             table.insert(args, s)
         end
     end
 
-    if #args <= 0 or args[1] == 'help' then
+    if #args <= 0 or args[1] == "help" then
         execHelp(user_peer_id, is_admin, is_auth, args)
-    elseif args[1] == 'on' then
+    elseif args[1] == "on" then
         execOn(user_peer_id, is_admin, is_auth, args)
-    elseif args[1] == 'off' then
+    elseif args[1] == "off" then
         execOff(user_peer_id, is_admin, is_auth, args)
-    elseif args[1] == 'spdofs' then
+    elseif args[1] == "spdofs" then
         execSpdOfs(user_peer_id, is_admin, is_auth, args)
-    elseif args[1] == 'altofs' then
+    elseif args[1] == "altofs" then
         execAltOfs(user_peer_id, is_admin, is_auth, args)
-    elseif args[1] == 'spdunit' then
+    elseif args[1] == "spdunit" then
         execSpdUnit(user_peer_id, is_admin, is_auth, args)
-    elseif args[1] == 'altunit' then
+    elseif args[1] == "altunit" then
         execAltUnit(user_peer_id, is_admin, is_auth, args)
     else
         server.announce(
@@ -65,13 +65,13 @@ function execHelp(user_peer_id, is_admin, is_auth, args)
     server.announce(
         getAnnounceName(),
         (
-            c_cmd .. ' on\n' ..
-            c_cmd .. ' off\n' ..
-            c_cmd .. ' spdofs HOFS VOFS\n' ..
-            c_cmd .. ' altofs HOFS VOFS\n' ..
-            c_cmd .. ' spdunit UNIT\n' ..
-            c_cmd .. ' altunit UNIT\n' ..
-            c_cmd .. ' help'
+            c_cmd .. " on\n" ..
+            c_cmd .. " off\n" ..
+            c_cmd .. " spdofs HOFS VOFS\n" ..
+            c_cmd .. " altofs HOFS VOFS\n" ..
+            c_cmd .. " spdunit UNIT\n" ..
+            c_cmd .. " altunit UNIT\n" ..
+            c_cmd .. " help"
         ),
         user_peer_id
     )
@@ -79,28 +79,28 @@ end
 
 function execOn(user_peer_id, is_admin, is_auth, args)
     if #args > 1 then
-        server.announce(getAnnounceName(), 'error: extra arguments', user_peer_id)
+        server.announce(getAnnounceName(), "error: extra arguments", user_peer_id)
         return
     end
-    g_userdata[user_peer_id]['enabled'] = true
-    server.announce(getAnnounceName(), 'widgets are now enabled', user_peer_id)
+    g_userdata[user_peer_id]["enabled"] = true
+    server.announce(getAnnounceName(), "widgets are now enabled", user_peer_id)
 end
 
 function execOff(user_peer_id, is_admin, is_auth, args)
     if #args > 1 then
-        server.announce(getAnnounceName(), 'error: extra arguments', user_peer_id)
+        server.announce(getAnnounceName(), "error: extra arguments", user_peer_id)
         return
     end
-    g_userdata[user_peer_id]['enabled'] = false
-    server.announce(getAnnounceName(), 'widgets are now disabled', user_peer_id)
+    g_userdata[user_peer_id]["enabled"] = false
+    server.announce(getAnnounceName(), "widgets are now disabled", user_peer_id)
 end
 
 function execSpdOfs(user_peer_id, is_admin, is_auth, args)
-    return execSetOfs(user_peer_id, is_admin, is_auth, args, 'spdofs', 'spd_hofs', 'spd_vofs')
+    return execSetOfs(user_peer_id, is_admin, is_auth, args, "spdofs", "spd_hofs", "spd_vofs")
 end
 
 function execAltOfs(user_peer_id, is_admin, is_auth, args)
-    return execSetOfs(user_peer_id, is_admin, is_auth, args, 'altofs', 'alt_hofs', 'alt_vofs')
+    return execSetOfs(user_peer_id, is_admin, is_auth, args, "altofs", "alt_hofs", "alt_vofs")
 end
 
 function execSetOfs(user_peer_id, is_admin, is_auth, args, param_name, param_key_hofs, param_key_vofs)
@@ -109,7 +109,7 @@ function execSetOfs(user_peer_id, is_admin, is_auth, args, param_name, param_key
             getAnnounceName(),
             string.format(
                 (
-                    'current %s is (%f, %f)\n' ..
+                    "current %s is (%f, %f)\n" ..
                     'use "%s %s HOFS VOFS" to configure'
                 ),
                 param_name,
@@ -122,7 +122,7 @@ function execSetOfs(user_peer_id, is_admin, is_auth, args, param_name, param_key
         )
         return
     elseif #args ~= 3 then
-        server.announce(getAnnounceName(), 'error: wrong number of arguments', user_peer_id)
+        server.announce(getAnnounceName(), "error: wrong number of arguments", user_peer_id)
         return
     end
 
@@ -145,7 +145,7 @@ function execSetOfs(user_peer_id, is_admin, is_auth, args, param_name, param_key
         )
         return
     elseif hofs < -1 or 1 < hofs or vofs < -1 or 1 < vofs then
-        server.announce(getAnnounceName(), 'error: offset must be within the range -1 to 1', user_peer_id)
+        server.announce(getAnnounceName(), "error: offset must be within the range -1 to 1", user_peer_id)
         return
     end
 
@@ -153,7 +153,7 @@ function execSetOfs(user_peer_id, is_admin, is_auth, args, param_name, param_key
     g_userdata[user_peer_id][param_key_vofs] = vofs
     server.announce(
         getAnnounceName(),
-        string.format('%s is now set to (%f, %f)', param_name, hofs, vofs),
+        string.format("%s is now set to (%f, %f)", param_name, hofs, vofs),
         user_peer_id
     )
 end
@@ -164,8 +164,8 @@ function execSpdUnit(user_peer_id, is_admin, is_auth, args)
         is_admin,
         is_auth,
         args,
-        'spdunit',
-        'spd_unit',
+        "spdunit",
+        "spd_unit",
         c_spd_unit_tbl,
         'available units are "km/h", "m/s", "kt"'
     )
@@ -177,8 +177,8 @@ function execAltUnit(user_peer_id, is_admin, is_auth, args)
         is_admin,
         is_auth,
         args,
-        'altunit',
-        'alt_unit',
+        "altunit",
+        "alt_unit",
         c_alt_unit_tbl,
         'available units are "m", "ft"'
     )
@@ -192,7 +192,7 @@ function execSetUnit(user_peer_id, is_admin, is_auth, args, param_name, param_ke
                 (
                     'current %s is "%s"\n' ..
                     'use "%s %s UNIT" to configure\n' ..
-                    '%s'
+                    "%s"
                 ),
                 param_name,
                 g_userdata[user_peer_id][param_key],
@@ -205,7 +205,7 @@ function execSetUnit(user_peer_id, is_admin, is_auth, args, param_name, param_ke
         return
     end
     if #args > 2 then
-        server.announce(getAnnounceName(), 'error: too many arguments', user_peer_id)
+        server.announce(getAnnounceName(), "error: too many arguments", user_peer_id)
         return
     end
 
@@ -229,15 +229,15 @@ end
 function onTick(game_ticks)
     local player_tbl = getPlayerTable()
     for _, player in pairs(player_tbl) do
-        if g_userdata[player['id']] == nil then
-            g_userdata[player['id']] = {
-                ['enabled'] = true,
-                ['spd_hofs'] = 0.8,
-                ['spd_vofs'] = -0.9,
-                ['spd_unit'] = 'km/h',
-                ['alt_hofs'] = 0.9,
-                ['alt_vofs'] = -0.8,
-                ['alt_unit'] = 'm',
+        if g_userdata[player["id"]] == nil then
+            g_userdata[player["id"]] = {
+                ["enabled"] = true,
+                ["spd_hofs"] = 0.8,
+                ["spd_vofs"] = -0.9,
+                ["spd_unit"] = "km/h",
+                ["alt_hofs"] = 0.9,
+                ["alt_vofs"] = -0.8,
+                ["alt_unit"] = "m",
             }
         end
     end
@@ -247,8 +247,8 @@ function onTick(game_ticks)
         end
     end
     for _, player in pairs(player_tbl) do
-        if g_usertemp[player['id']] == nil then
-            g_usertemp[player['id']] = {}
+        if g_usertemp[player["id"]] == nil then
+            g_usertemp[player["id"]] = {}
         end
     end
     for peer_id, _ in pairs(g_usertemp) do
@@ -258,7 +258,7 @@ function onTick(game_ticks)
     end
 
     for peer_id, _ in pairs(g_userdata) do
-        if not g_userdata[peer_id]['enabled'] then
+        if not g_userdata[peer_id]["enabled"] then
             g_usertemp[peer_id] = {}
             goto continue
         end
@@ -267,9 +267,9 @@ function onTick(game_ticks)
         if not is_success then
             vehicle_id = nil
         end
-        if vehicle_id ~= g_usertemp[peer_id]['vehicle_id'] then
+        if vehicle_id ~= g_usertemp[peer_id]["vehicle_id"] then
             g_usertemp[peer_id] = {
-                ['vehicle_id'] = vehicle_id,
+                ["vehicle_id"] = vehicle_id,
             }
         end
 
@@ -280,14 +280,14 @@ function onTick(game_ticks)
                 goto continue
             end
 
-            if g_usertemp[peer_id]['vehicle_pos'] ~= nil then
-                g_usertemp[peer_id]['spd'] = matrix.distance(vehicle_pos, g_usertemp[peer_id]['vehicle_pos'])
+            if g_usertemp[peer_id]["vehicle_pos"] ~= nil then
+                g_usertemp[peer_id]["spd"] = matrix.distance(vehicle_pos, g_usertemp[peer_id]["vehicle_pos"])
             end
-            g_usertemp[peer_id]['alt'] = table.pack(matrix.position(vehicle_pos))[2]
-            g_usertemp[peer_id]['vehicle_pos'] = vehicle_pos
+            g_usertemp[peer_id]["alt"] = table.pack(matrix.position(vehicle_pos))[2]
+            g_usertemp[peer_id]["vehicle_pos"] = vehicle_pos
         else
-            local player_log = g_usertemp[peer_id]['player_log'] or {}
-            local player_log_idx = g_usertemp[peer_id]['player_log_idx'] or 1
+            local player_log = g_usertemp[peer_id]["player_log"] or {}
+            local player_log_idx = g_usertemp[peer_id]["player_log_idx"] or 1
             local player_log_len = peer_id == 0 and 2 or 61
 
             local player_object_id, is_success = server.getPlayerCharacterID(peer_id)
@@ -306,11 +306,11 @@ function onTick(game_ticks)
 
             if #player_log > 1 then
                 local player_old_pos = player_log[player_log_idx] or player_log[1]
-                g_usertemp[peer_id]['spd'] = matrix.distance(player_old_pos, player_new_pos)/(#player_log - 1)
+                g_usertemp[peer_id]["spd"] = matrix.distance(player_old_pos, player_new_pos)/(#player_log - 1)
             end
-            g_usertemp[peer_id]['alt'] = table.pack(matrix.position(player_new_pos))[2]
-            g_usertemp[peer_id]['player_log'] = player_log
-            g_usertemp[peer_id]['player_log_idx'] = player_log_idx
+            g_usertemp[peer_id]["alt"] = table.pack(matrix.position(player_new_pos))[2]
+            g_usertemp[peer_id]["player_log"] = player_log
+            g_usertemp[peer_id]["player_log_idx"] = player_log_idx
         end
         ::continue::
     end
@@ -318,60 +318,71 @@ function onTick(game_ticks)
     for peer_id, _ in pairs(g_userdata) do
         local userdata = g_userdata[peer_id]
         local usertemp = g_usertemp[peer_id]
-        if not userdata['enabled'] then
+        if not userdata["enabled"] then
             goto continue
         end
 
-        local spdtxt = 'SPD\n---'
-        if usertemp['spd'] ~= nil then
+        local spdtxt = "SPD\n---"
+        if usertemp["spd"] ~= nil then
             spdtxt = string.format(
-                'SPD\n%.2f%s',
-                usertemp['spd']*c_spd_unit_tbl[userdata['spd_unit']],
-                userdata['spd_unit']
+                "SPD\n%.2f%s",
+                usertemp["spd"]*c_spd_unit_tbl[userdata["spd_unit"]],
+                userdata["spd_unit"]
             )
         end
 
-        local alttxt = 'ALT\n---'
-        if usertemp['alt'] ~= nil then
+        local alttxt = "ALT\n---"
+        if usertemp["alt"] ~= nil then
             alttxt = string.format(
-                'ALT\n%.2f%s',
-                usertemp['alt']*c_alt_unit_tbl[userdata['alt_unit']],
-                userdata['alt_unit']
+                "ALT\n%.2f%s",
+                usertemp["alt"]*c_alt_unit_tbl[userdata["alt_unit"]],
+                userdata["alt_unit"]
             )
         end
 
-        g_uim:setPopupScreen(peer_id, g_savedata['spd_ui_id'], getAnnounceName(), true, spdtxt, userdata['spd_hofs'], userdata['spd_vofs'])
-        g_uim:setPopupScreen(peer_id, g_savedata['alt_ui_id'], getAnnounceName(), true, alttxt, userdata['alt_hofs'], userdata['alt_vofs'])
+        g_uim:setPopupScreen(peer_id, g_savedata["spd_ui_id"], getAnnounceName(), true, spdtxt, userdata["spd_hofs"], userdata["spd_vofs"])
+        g_uim:setPopupScreen(peer_id, g_savedata["alt_ui_id"], getAnnounceName(), true, alttxt, userdata["alt_hofs"], userdata["alt_vofs"])
         ::continue::
     end
     g_uim:flushPopup()
 
     if g_userdata[0] ~= nil then
-        g_savedata['hostdata'] = deepcopy(g_userdata[0])
+        g_savedata["hostdata"] = deepcopy(g_userdata[0])
     end
 end
 
 function onCreate(is_world_create)
     local version = 1
-    if g_savedata['version'] ~= version then
+    if g_savedata["version"] ~= version then
         g_savedata = {
-            ['version'] = version,
-            ['spd_ui_id'] = server.getMapID(),
-            ['alt_ui_id'] = server.getMapID(),
-            ['hostdata'] = nil,
+            ["version"] = version,
+            ["spd_ui_id"] = server.getMapID(),
+            ["alt_ui_id"] = server.getMapID(),
+            ["hostdata"] = nil,
         }
     end
-    if g_savedata['hostdata'] ~= nil then
-        g_userdata[0] = deepcopy(g_savedata['hostdata'])
+    if g_savedata["hostdata"] ~= nil then
+        g_userdata[0] = deepcopy(g_savedata["hostdata"])
     end
 
     g_uim = buildUIManager()
-    server.removePopup(-1, g_savedata['spd_ui_id'])
-    server.removePopup(-1, g_savedata['alt_ui_id'])
+    server.removePopup(-1, g_savedata["spd_ui_id"])
+    server.removePopup(-1, g_savedata["alt_ui_id"])
 end
 
 function onPlayerJoin(steam_id, name, peer_id, is_admin, is_auth)
     g_uim:onPlayerJoin(steam_id, name, peer_id, is_admin, is_auth)
+end
+
+function init()
+    g_userdata = {}
+    g_spd_ui_id = nil
+    g_alt_ui_id = nil
+    g_uim = buildUIManager()
+
+    local savedata = deepcopy(g_savedata)
+    if type(savedata) == "table" then
+    end
 end
 
 function buildUIManager()
@@ -386,7 +397,7 @@ function buildUIManager()
             return
         end
 
-        local key = string.pack('jj', peer_id, ui_id)
+        local key = string.pack("jj", peer_id, ui_id)
         self._popup_new[key] = {
             peer_id = peer_id,
             ui_id = ui_id,
@@ -456,7 +467,7 @@ function getAddonName()
 end
 
 function getAnnounceName()
-    return string.format('[%s]', getAddonName())
+    return string.format("[%s]", getAddonName())
 end
 
 function getPlayerTable()
@@ -476,7 +487,7 @@ function getPlayerVehicle(peer_id)
 end
 
 function deepcopy(v)
-    if type(v) ~= 'table' then
+    if type(v) ~= "table" then
         return v
     end
 
