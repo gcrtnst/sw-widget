857953a41714cf39044d77054ce1ef4e2c210525
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-03-17 20:35:15 +0900

    Remove `tracker:getVehicleSpdAlt`

--- widget/script.lua
+++ widget/script.lua
@@ -460,26 +460,6 @@ function buildTracker()
         self._player_pos_new = {}
     end
 
-    function tracker:getVehicleSpdAlt(vehicle_id)
-        local vehicle_pos_new = self._vehicle_pos_new[vehicle_id]
-        if vehicle_pos_new == nil then
-            local is_success
-            vehicle_pos_new, is_success = server.getVehiclePos(vehicle_id)
-            if not is_success then
-                return nil, nil
-            end
-            self._vehicle_pos_new[vehicle_id] = vehicle_pos_new
-        end
-
-        local spd = nil
-        local _, alt, _ = matrix.position(vehicle_pos_new)
-        local vehicle_pos_old = self._vehicle_pos_old[vehicle_id]
-        if vehicle_pos_old ~= nil then
-            spd = matrix.distance(vehicle_pos_old, vehicle_pos_new)
-        end
-        return spd, alt
-    end
-
     function tracker:getVehicleWorldSpdPos(vehicle_id)
         local vehicle_pos_new = self._vehicle_pos_new[vehicle_id]
         if vehicle_pos_new == nil then
