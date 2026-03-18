a311faa744a11d71c2b329d0f0a05f5f10c9eea4
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-03-19 01:48:15 +0900

    Bump version to v0.2.0

--- widget/script_test.lua
+++ widget/script_test.lua
@@ -157,7 +157,7 @@ function test_decl.testOnCustomCommandWidgetHelp(t)
 end
 
 function test_decl.testOnCustomCommandWidgetVersion(t)
-    local ver = "v0.2.0-beta.1"
+    local ver = "v0.2.0"
     local tt = {
         {
             prefix = "host",
